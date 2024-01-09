350be3dd94efc7112081e3381f6c43466a47e150
MichelHdez <michel01406@gmail.com>
2024-01-08 18:37:35 -0600

    [UPDATE] actualización archivos paginación-busqueda

--- src/main/java/com/gestioncursos/controller/CursoController.java
+++ src/main/java/com/gestioncursos/controller/CursoController.java
@@ -6,16 +6,22 @@ import com.gestioncursos.reports.ExporterPDF;
 import com.gestioncursos.repository.CursoRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -31,21 +37,35 @@ public class CursoController {
         return "redirect:/cursos";
     }
 
-    @GetMapping("/cursos")
-    public String listaCursos(Model model) {
-        List<Curso> cursos;
-        cursos = cursoRepository.findAll();
-        model.addAttribute("cursos", cursos);
-        return "cursos";
-    }
+   @GetMapping("/cursos")
+    public String listarCursos(Model model, @Param("keyword") String keyword, @RequestParam(defaultValue = "1") int page,@RequestParam(defaultValue = "3") int size){
+        try{
+            List<Curso> cursos = new ArrayList<>();
+            Pageable paging = PageRequest.of(page-1,size);
 
-    @GetMapping("/cursos/nuevo")
-    public String agregarCursos(Model model) {
-        Curso curso = new Curso();
-        curso.setPublicado(true);
-        model.addAttribute("curso", curso);
-        model.addAttribute("pageTitle", "Nuevo curso");
-        return "cursoForm";
+            Page<Curso> pageCursos = null;
+
+            if(keyword == null){
+                pageCursos = cursoRepository.findAll(paging);
+            }
+            else{
+                pageCursos = cursoRepository.findByTituloContainingIgnoreCase(keyword,paging);
+                model.addAttribute("keyword",keyword);
+            }
+
+            cursos = pageCursos.getContent();
+
+            model.addAttribute("cursos",cursos);
+
+            model.addAttribute("currentPage",pageCursos.getNumber() + 1);
+            model.addAttribute("totalItems",pageCursos.getTotalElements());
+            model.addAttribute("totalPages",pageCursos.getTotalPages());
+            model.addAttribute("pageSize",size);
+        }catch (Exception exception){
+            model.addAttribute("message",exception.getMessage());
+        }
+
+        return "cursos";
     }
 
     @PostMapping("/cursos/guardar")
@@ -91,7 +111,7 @@ public class CursoController {
 
         String headerKey = "Content-Disposition";
         String headerValue = "attachment; filename=cursos" + currentDateTime + ".pdf";
-        response.setHeader(headerKey,headerValue);
+        response.setHeader(headerKey, headerValue);
 
         List<Curso> cursos = cursoRepository.findAll();
 
@@ -107,7 +127,7 @@ public class CursoController {
 
         String headerKey = "Content-Disposition";
         String headerValue = "attachment; filename=cursos" + currentDateTime + ".xlsx";
-        response.setHeader(headerKey,headerValue);
+        response.setHeader(headerKey, headerValue);
 
         List<Curso> cursos = cursoRepository.findAll();
 
